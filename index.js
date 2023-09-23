0847b561b510bb3354923a31f1c37684f1015071
Samuel Kimote <readssamuel@.com>
2023-09-23 16:30:30 +0300

    complete

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// multiply 
 let num1 = 31,
     num2 = 2,
     num3 = 10,
     num4 = 6;
 let multiply = num1 * num2;
-
+// Random number 
 let random = Math.floor(Math.random() * 10) + 1;
 
 let mod = num3 % num4;
